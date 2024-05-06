669f95de3570e1cbd52f64eac9fecd632581ef3b
Roland Conybeare <rconybeare@gmail.com>
2024-05-06 10:04:12 -0400

    cr: drop unused build inputs

--- pkgs/development/python-modules/sphinxcontrib-ditaa/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-ditaa/default.nix
@@ -18,12 +18,11 @@ buildPythonPackage rec {
     sha256 = "57f2e13b059b38fde89580beca916ac6ca7456b2d706c3ddea9dd4890e5f5bd3";
   };
 
-  buildInputs = [ mock sphinx-testing ];
   dependencies = [ sphinx ditaa ];
 
   # no tests provided
   doCheck = false;
-  
+
   pythonImportsCheck = [ "sphinxcontrib-ditaa" ];
 
   pythonNamespaces = [ "sphinxcontrib" ];
